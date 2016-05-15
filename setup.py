50be07a96bfa707c2ee3ed9bda13103f3ab340cd
Henry Kupty <hkupty@gmail.com>
2016-05-15 12:35:21 -0300

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='asterix',
-    version='0.0.1',
+    version='0.0.2',
     description=' Manage python components startup quickly and efficiently',
     long_description=long_description,
     url='https://github.com/hkupty/asterix',
