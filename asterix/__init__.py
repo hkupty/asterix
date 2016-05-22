1b41bb5b034aa3e60f64149f5d6181b4064a722c
Henry Kupty <hkupty@gmail.com>
2016-05-22 17:35:37 -0300

    Rename main file to core

--- asterix/__init__.py
+++ asterix/__init__.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from asterix.asterix import start_system, get_component, component
+from asterix.core import start_system, get_component, component
 
 __all__ = [
 'start_system',
