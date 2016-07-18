c8ce8f2b485d01a0df42ccfa1cf406c418ba68f1
Henry Kupty <hkupty@gmail.com>
2016-07-17 22:16:26 -0300

    Reuse and allow checking mock stuff

--- asterix/test.py
+++ asterix/test.py
@@ -5,8 +5,14 @@ from unittest.mock import Mock
 
 class dummy(object):
 
+    def __init__(self):
+        self.components = {}
+
     def get(self, name, default):
-        return Mock()
+        if name not in self.components:
+            self.components[name] = Mock()
+
+        return self.components[name]
 
 
 class dummy_master(object):
